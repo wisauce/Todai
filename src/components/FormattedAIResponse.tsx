f4f22b55a4b4c0b25de38c35b3cd132f239ab532
wisaahmaduta <wiseowisa@gmail.com>
2025-05-30 22:53:02 +0700

    Initial commit

---
+++ src/components/FormattedAIResponse.tsx
@@ -0,0 +1,58 @@
+'use client'
+
+import React, { JSX } from 'react'
+
+type Props = {
+  text: string
+}
+
+export default function FormattedAIResponse({ text }: Props) {
+  const lines = text.split('\n')
+  const elements: JSX.Element[] = []
+  let currentList: string[] = []
+
+  const flushList = () => {
+    if (currentList.length > 0) {
+      elements.push(
+        <ul className="list-disc pl-6 mb-4" key={elements.length}>
+          {currentList.map((item, idx) => (
+            <li key={idx} dangerouslySetInnerHTML={{ __html: formatInline(item) }} />
+          ))}
+        </ul>
+      )
+      currentList = []
+    }
+  }
+
+  const formatInline = (line: string): string => {
+    return line
+      .replace(/\*\*(.*?)\*\*/g, '<strong>$1</strong>')
+      .replace(/\*(.*?)\*/g, '<em>$1</em>')
+  }
+
+  lines.forEach((line, idx) => {
+    line = line.trim()
+
+    if (line.startsWith('###')) {
+      flushList()
+      elements.push(
+        <h3 className="text-lg font-semibold mt-4 mb-1" key={idx}>
+          {line.replace(/^###\s*/, '')}
+        </h3>
+      )
+    } else if (line.startsWith('-')) {
+      currentList.push(line.replace(/^- /, ''))
+    } else if (line === '') {
+      flushList()
+    } else {
+      flushList()
+      elements.push(
+        <p className="mb-2" key={idx} dangerouslySetInnerHTML={{ __html: formatInline(line) }} />
+      )
+    }
+  })
+
+  flushList()
+
+  return <div className="prose max-w-none">{elements}</div>
+}
